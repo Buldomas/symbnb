73dc2e0cf1f0b0205b54d184804b8a0b8345d697
Buldomas <laurent.soubigou445@orange.fr>
2020-06-15 14:06:16 +0200

    Premier commi: installation et page d'accueil

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/hello/{prenom}/age/{age}", name="hello")
+     * @Route("/hello", name="hello_base")
+     * @Route("/hello/{prenom}", name="hello_prenom")
+     * 
+     * @return void
+     */
+    public function hello($prenom = "anonyme", $age = 0)
+    {
+        return $this->render(
+            'hello.html.twig',
+            [
+                'prenom' => $prenom,
+                'age' => $age
+            ]
+        );
+    }
+    /**
+     * @Route("/", name="homepage")
+     *
+     */
+    public function home()
+    {
+        $prenoms = ["Lior" => 31, "Joseph" => 12, "Anne" => 55];
+        return $this->render(
+            'home.html.twig',
+            [
+                'title' => "Bonjour à tous",
+                'age' => 8,
+                'tableau' => $prenoms
+            ]
+        );
+    }
+}
