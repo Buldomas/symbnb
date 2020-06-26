2ea02bc841513ca1c118ef3d6daeb8259cf688d3
Buldomas <laurent.soubigou445@orange.fr>
2020-06-26 09:43:27 +0200

    Modification via OpenClassRoom

--- webpack.config.js
+++ webpack.config.js
@@ -24,6 +24,7 @@ Encore
 	 * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
 	 */
 	.addEntry("app", "./assets/js/app.js")
+	.addEntry("app/css", "./assets/css/app.css")
 	.addEntry("css/app", "./assets/css/app.css")
 	//.addEntry('page2', './assets/js/page2.js')
 
